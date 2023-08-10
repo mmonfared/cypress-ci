3588d8cfb860b45823f98807a06434b01cbbe12a
Mohammad Monfared <engineer2012@gmail.com>
2023-08-10 23:09:32 +0400

    first

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,10 @@
+describe("test suite", () => {
+    it('01-Passing test', function (){
+        cy.visit('https://play1.automationcamp.ir/forms.html')
+        cy.get('#notes').type('Automation Camp').should('have.value', 'Automation Camp')
+    })
+    it('02-Failing test', function (){
+        cy.visit('https://play1.automationcamp.ir/forms.html')
+        cy.get('#notes').type('Automation Camp').should('have.value', 'AutomationCamp')
+      })
+})
